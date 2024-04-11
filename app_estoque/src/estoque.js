05b0a9c70da24c1a6b2b6a70275c79ddc7cf204d
SarahFatimaDi <106893488+SarahFatimaDi@users.noreply.github.com>
2024-04-11 11:23:23 -0300

    Add files via upload

---
+++ app_estoque/src/estoque.js
@@ -0,0 +1,30 @@
+let produtos = [];
+
+function criarProduto(id, nome, qtd){
+    let p = {
+        id: id,
+        nome: nome,
+        qtd: qtd
+    };
+    return p;
+}
+
+function adicionarProduto(p){
+    produtos.push(p); //vai armazenar na var. da linha 1
+}
+
+function listarProdutos(){
+    return produtos;
+}
+
+function removerProduto(id){
+    produtos = produtos.filter((p) => {
+        return p.id != id;
+    });
+}
+
+module.exports = {
+    criarProduto,
+    adicionarProduto,
+    listarProdutos
+};
